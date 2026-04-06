14fb28991d7d260e41d61cc546fe284c9a16d336
metalsquid <bslowinskird@gmail.com>
2026-04-06 15:53:59 -0400

    adjusted data

--- app/components/directory-item/directory-item.component.jsx
+++ app/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router";
 import "./directory-item.styles.css";
 
-export default function DirectoryItem({category}){
-    const {imageUrl, title, route } = category
-    
-    return (
-        <Link to={route} className="directory-item-container">
-            <img className="background-image" src={imageUrl}/>
-            
-                <h2>{title}</h2>
-                <p>shop now</p>
-            
-        </Link>
-    )
+export default function DirectoryItem({ category }) {
+  const { imageUrl, title, route, name } = category;
+
+  return (
+    <Link to={route} className="directory-item-container">
+      <img className="background-image" src={imageUrl} alt={name} />
+
+      <h2>{title}</h2>
+      <p>shop now</p>
+    </Link>
+  );
 }
